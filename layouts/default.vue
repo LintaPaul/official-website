807eb858a14329652966bd9e855b543e6fe41a7f
LintaPaul <54324010+LintaPaul@users.noreply.github.com>
2020-05-16 12:32:08 +0530

    fixed issue #Add Committee
    changes made to ./ pages/committee/index.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,7 +7,6 @@
 
 <script>
 import Header from '~/components/Header'
-
 export default {
   components: {
     Header
@@ -15,5 +14,6 @@ export default {
 }
 </script>
 
-<style>
+<style lang='scss'>
+
 </style>
